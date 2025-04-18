6209eac00ede1126adebda930425037bea362e92
Manu R <manurajbhar12@gmail.com>
2025-04-18 18:35:04 +0530

    some changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-#![no_std]
+#![no_std]       
+#![no_main]      
 
-fn main() {}
+use core::panic::PanicInfo;
+
+/// This function is called when a panic occurs.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+/// Our custom entry point
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {} // Loop forever
+}
---
+++ target/debug/deps/RustOS-466332f62f888f5f.d
@@ -0,0 +1,5 @@
+/Users/manu/Documents/RustOS/target/debug/deps/RustOS-466332f62f888f5f: src/main.rs
+
+/Users/manu/Documents/RustOS/target/debug/deps/RustOS-466332f62f888f5f.d: src/main.rs
+
+src/main.rs:
